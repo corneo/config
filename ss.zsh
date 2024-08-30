9a30285a8866d10d424e3a459d3f526969f683ba
corneo <976097+corneo@users.noreply.github.com>
2024-08-30 11:46:56 -0400

    Update ss.zsh

--- ss.zsh
+++ ss.zsh
@@ -1,18 +1,15 @@
-echo "--- env | grep 'PASSWORD' results:"
-env | grep 'PASSWORD'
+echo "--- env | grep '_PASSWORD' results:"
+env | grep '_PASSWORD'
 echo "---"
-
-if [ -n "${OTA_PASSWORD+1}" ]; then
-  echo "OTA_PASSWORD is defined"
-else
-  echo "OTA_PASSWORD is not defined"
-fi
 
 env_file=config/secrets.env
 env_path=$HOME/$env_file
 
 if [[ -r "$env_path" ]]; then
   echo "$env_file exists and is readable (-r)"
+  echo "--- $env_path file contents:"
+  cat $env_path
+  echo "---"
 else
   echo "$env_file does not exist and/or is not readable (! -r)"
 fi
